8f559fa36735b1d465d8721e56d92a13899a4b12
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-06-17 10:57:48 -0300

    Finalizacion de constructor con su complejidad

--- estudiantes/src/main/java/aed/SistemaSIU.java
+++ estudiantes/src/main/java/aed/SistemaSIU.java
@@ -11,12 +11,35 @@ public class SistemaSIU {
         JTP,
         PROF
     }
-
+    //O(Sumatoria desde 1 a |M| de (Sumatoria desde 1 a |C| de (|c|+|Mc| + E)
     public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias){
+        // O(E)
         for (String libreta : libretasUniversitarias){
             Alumno alumno = new Alumno(libreta);
             this.alumnos.agregar(libreta, alumno);
         } 
+        //O(Sumatoria desde 1 a |M| de (Sumatoria desde 1 a |C| de (|c|+|Mc|) 
+        for (InfoMateria infomateria : infoMaterias){
+            Materia materia = new Materia(infomateria);
+            /*O(Sumatoria desde 1 a |Nm| de (|c|+|Mc|) pero nos quedamos con esta 
+            O(Sumatoria desde 1 a |C| de (|c|+|Mc|  porque es cota superior,
+            porque vale esto |Carreras| >= |Nm|)*/
+            for (ParCarreraMateria par : infomateria.getParesCarreraMateria()) {
+                //O(|c|+|Mc|)              
+                if (carreras.pertenece(par.getCarrera())) {
+
+                    Carrera carrera = carreras.obtener(par.getCarrera());
+                    carrera.getMaterias().agregar(par.getNombreMateria(),materia);
+
+                }
+                else{
+
+                    Carrera carrera = new Carrera();
+                    this.carreras.agregar(par.getCarrera(),carrera);
+                    carrera.getMaterias().agregar(par.getNombreMateria(),materia);
+                }
+                }
+        }
     }
 
     public void inscribir(String estudiante, String carrera, String materia){
