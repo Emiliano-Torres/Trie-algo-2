eb3a6d47df82ee840b95a4921c9c3fa87cc9dc6c
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-06-20 11:20:09 -0300

    archivo alumnos completo

--- estudiantes/src/main/java/aed/Alumno.java
+++ estudiantes/src/main/java/aed/Alumno.java
@@ -14,26 +14,30 @@ public class Alumno {
     private String LU;
 
     // Constructor
+    //O(1)
     public Alumno(String LU) {
         inscripciones = 0;
         this.LU = LU;
     }
 
     // Metodo para obtener la cantidad de materias en las que esta inscripto el alumno
+    //O(1)
     public int obtenerInscripciones() {
         return inscripciones;
     }
 
     // Metodo para aumentar la cantidad de materias en las que esta inscripto el alumno
+    //O(1)
     public void aumentarInscripciones() {
         inscripciones +=1 ;
     }
 
     // Metodo para obtener la L.U del alumno
+    //O(1)
     public String obtenerLU() {
         return LU;
     }
-
+    //O(1)
     public void reducirInscripciones() {
         inscripciones -=1 ;
     }
