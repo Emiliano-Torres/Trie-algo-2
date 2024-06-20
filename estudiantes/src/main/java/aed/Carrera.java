5a055ec33d7662e08bdee603875b3e275685b782
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-06-20 11:29:55 -0300

    archivo materia y carrera completo

--- estudiantes/src/main/java/aed/Carrera.java
+++ estudiantes/src/main/java/aed/Carrera.java
@@ -2,9 +2,9 @@ package aed;
 /*INV REP : (paraToda infomateria en infomaterias)(paraTodo parCarreraMateria en infomateria)(si la carrera "c" esta dentro de la infomateria todas
              las materias que estan en el trie tienen a "c" como carrera)*/
 public class Carrera {
-    
+    //O(1)
     private Trie<Materia> materias = new Trie<>();
-
+    //O(1)
     public Trie<Materia> getMaterias(){
         return this.materias;
     }
