7b1206dbc60f269c8ebbd7fb3cb068ab8fd1a822
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-06-20 15:42:55 -0300

    Tp finalizado

--- estudiantes/src/main/java/aed/Carrera.java
+++ estudiantes/src/main/java/aed/Carrera.java
@@ -1,6 +1,6 @@
 package aed;
-/*INV REP : (paraToda infomateria en infomaterias)(paraTodo parCarreraMateria en infomateria)(si la carrera "c" esta dentro de la infomateria todas
-             las materias que estan en el trie tienen a "c" como carrera)*/
+/*INV REP : (paraToda infomateria en infomaterias)(paraTodo parCarreraMateria en infomateria)(si la carrera "c" esta dentro de la infomateria 
+            todas las materias que estan en el trie tienen a "c" como carrera)*/
 public class Carrera {
     //O(1)
     private Trie<Materia> materias = new Trie<>();
--- estudiantes/src/main/java/aed/SistemaSIU.java
+++ estudiantes/src/main/java/aed/SistemaSIU.java
@@ -17,7 +17,7 @@ public class SistemaSIU {
         JTP,
         PROF
     }
-    //O(Sumatoria desde 1 a |M| de (Sumatoria desde 1 a |C| de (|c|+|Mc| + E)
+    //O(Sumatoria c en C de(|c|*|Mc|) + sumatoria m en M de(sumatoria n en Nm de(|n|))+ E)
     public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias){
         
         // O(E)
@@ -26,204 +26,182 @@ public class SistemaSIU {
             Alumno alumno = new Alumno(libreta);
             alumnos.agregar(libreta, alumno);
         }
-        
-        for (InfoMateria infoMateria: infoMaterias) {
+        /*  O(Sumatoria m en M de(sumatoria n en Nm de(|c|)) + sumatoria m en M de(sumatoria n en Nm de(|n|))
+         *
+         * Luego sumar todos los nombres de todas las materias es lo mismo que sumar todos los nombres por carrera para todas las carreras.
+         *  
+         *  O(Sumatoria m en M de(sumatoria n en Nm de(|c|)) = O(Sumatoria c en C de(sumatoria mc en Mc de(|c|))
+         *  
+         * Luego |c| no depende de mc en Mc, sale multiplicando de la sumatoria
+         * 
+         *  O(Sumatoria m en M de(sumatoria n en Nm de(|c|))=O(Sumatoria c en C de(|c|*sumatoria mc en Mc de(1))
+         *  
+         * Luego:
+         *  
+         * O(Sumatoria m en M de(sumatoria n en Nm de(|c|))=O(Sumatoria c en C de(|c|*|Mc|)
+        */
+        for (InfoMateria infoMateria: infoMaterias/*|M|*/) {
             // Iterar sobre cada ParCarreraMateria en InfoMateria
-            Materia materiaNueva = new Materia(infoMateria);
-            Lista_enlazada<Carrera> lista_carreras = materiaNueva.getCarreras();
-            for (ParCarreraMateria par: infoMateria.getParesCarreraMateria()) {
+            Materia materiaNueva = new Materia(infoMateria);//O(1)
+            Lista_enlazada<Carrera> lista_carreras = materiaNueva.getCarreras();//O(1)
+
+            /*O(sumatoria n en Nm de(|c|+|n|) */
+            for (ParCarreraMateria par: infoMateria.getParesCarreraMateria()/*|Nm|*/) {
                 String carrera = par.getCarrera();
                 String materia = par.getNombreMateria();
-
                 // Verificar si la carrera ya existe en el trie de carreras
-                if (carreras.pertenece(carrera)) {
+                if (carreras.pertenece(carrera) /*|c|*/) {
                     // Si existe, obtener la carrera y agregar la materia
-                    Carrera carreraExistente = carreras.obtener(carrera);
-                    carreraExistente.getMaterias().agregar(materia, materiaNueva);
-                    lista_carreras.agregarAdelante(carreraExistente);
+                    Carrera carreraExistente = carreras.obtener(carrera); //O(c)
+                    carreraExistente.getMaterias().agregar(materia, materiaNueva); //O(n)
+                    lista_carreras.agregarAdelante(carreraExistente); //O(1)
                 } else{
                     // Si no existe, crear una nueva carrera y agregar la materia
-                    Carrera nuevCarrera = new Carrera();
-                    nuevCarrera.getMaterias().agregar(materia, materiaNueva);
-                    carreras.agregar(carrera, nuevCarrera);
-                    lista_carreras.agregarAdelante(nuevCarrera);
+                    Carrera nuevCarrera = new Carrera(); //O(1)
+                    nuevCarrera.getMaterias().agregar(materia, materiaNueva); //O(n)
+                    carreras.agregar(carrera, nuevCarrera); //O(c)
+                    lista_carreras.agregarAdelante(nuevCarrera); //O(1)
                 }
                 
             }
         }
-
-        /* 
-        //O(Sumatoria desde 1 a |M| de (Sumatoria desde 1 a |C| de (|c|+|Mc|) 
-        for (int i =0; i<infoMaterias.length;i++){
-            Materia materia = new Materia(infoMaterias[i]);
-            Lista_enlazada<Carrera> lista_carreras = materia.getCarreras();
-            // O(Sumatoria desde 1 a |Nm| de (|c|+|Mc|) pero nos quedamos con esta 
-            // O(Sumatoria desde 1 a |C| de (|c|+|Mc|  porque es cota superior,
-            // porque vale esto |Carreras| >= |Nm|)
-            for (ParCarreraMateria par : infoMaterias[i].getParesCarreraMateria()) {
-                //tomamos como peor caso que la carrera no este en el sistema
-                //O(|c|+|Mc|)              
-                if (carreras.pertenece(par.getCarrera())) {
-
-                    Carrera carrera = carreras.obtener(par.getCarrera());
-                    carrera.getMaterias().agregar(par.getNombreMateria(),materia);
-                    lista_carreras.agregarAdelante(carrera);
-                    
-                }
-                else{
-                    Carrera carrera = new Carrera();
-                    this.carreras.agregar(par.getCarrera(),carrera);
-                    carrera.getMaterias().agregar(par.getNombreMateria(),materia);
-                    lista_carreras.agregarAdelante(carrera);
-                }
-                }
-        }*/
     }
-    //O(|carrera|+|materia|)
+    //O(|c|+|m|)
     public void inscribir(String estudiante, String carrera, String materia){
-        //O(1)
-        Alumno inscripto=this.alumnos.obtener(estudiante);
+        
+        Alumno inscripto=this.alumnos.obtener(estudiante); //O(1)
         inscripto.aumentarInscripciones();
 
-        //O(|carrera|)
-        Trie <Materia> materias = this.carreras.obtener(carrera).getMaterias();
-        //O(|materia|)
-        materias.obtener(materia).getAlumnos().agregarAdelante(estudiante);
+        Trie <Materia> materias = this.carreras.obtener(carrera).getMaterias(); //O(|c|)
+        materias.obtener(materia).getAlumnos().agregarAdelante(estudiante); //O(|m|)
     }
-    //O(|carrera|+|materia|)
+
+    //O(|c|+|m|)
     public void agregarDocente(CargoDocente cargo, String carrera, String materia){
-        //O(|carrera|)
-        Trie <Materia> materias = this.carreras.obtener(carrera).getMaterias();
-        //O(|materia|)
-        int[] profesores = materias.obtener(materia).getProfesores();
-        //O(1)
-        if (cargo==CargoDocente.AY2) {
+        
+        Trie <Materia> materias = this.carreras.obtener(carrera).getMaterias();  //O(|c|)
+        int[] profesores = materias.obtener(materia).getProfesores(); //O(|m|)
+        
+        if (cargo==CargoDocente.AY2) { //O(1)
             profesores[3]++;
         }
-        //O(1)
-        if (cargo==CargoDocente.AY1) {
+        
+        if (cargo==CargoDocente.AY1) { //O(1)
             profesores[2]++;
         }
-        //O(1)
-        if (cargo==CargoDocente.JTP) {
+        
+        if (cargo==CargoDocente.JTP) { //O(1)
             profesores[1]++;
         }
-        //O(1)
-        if (cargo==CargoDocente.PROF) {
+        
+        if (cargo==CargoDocente.PROF) { //O(1)
             profesores[0]++;
         }	    
     }
-    //O(|carrera|+|materia|)
+
+    //O(|c|+|m|)
     public int[] plantelDocente(String materia, String carrera){
-        //O(|carrera|)
-        Trie <Materia> materias = this.carreras.obtener(carrera).getMaterias();
-        //O(|materia|)
-        return materias.obtener(materia).getProfesores();
+        
+        Trie <Materia> materias = this.carreras.obtener(carrera).getMaterias(); //O(|c|)
+        return materias.obtener(materia).getProfesores();  //O(|m|)
     }
-    // O(|carrera| + |nombre de la materia|+Em+Sumatoria para todo n en Nm de |n|)
+
+    // O(|c| + |n| + Em + Sumatoria para todo n en Nm de |n|)
     public void cerrarMateria(String materia, String carrera){
-        //O(|carrera| + |nombre de la materia|)
-        Materia materia_a_borrar=this.carreras.obtener(carrera).getMaterias().obtener(materia);
-        //O(1)
-        ParCarreraMateria[] Lista_nombres = materia_a_borrar.getInfoMateria().getParesCarreraMateria();
-        //O(1)
-        Lista_enlazada<String> lista = materia_a_borrar.getAlumnos();
-        //O(Em)
-        int cantidad_alumnos_a_desinscribir=lista.longitud();
-        for (int i = 0; i < cantidad_alumnos_a_desinscribir; i++){
-            //O(1)
-            String lu = (String)lista.obtenerUltimo();
-            //O(1)
-            alumnos.obtener(lu).reducirInscripciones();
-            //O(1)
-            lista.eliminar_ultimo();
+        
+        Materia materia_a_borrar=this.carreras.obtener(carrera).getMaterias().obtener(materia); //O(|c| + |n|)
+        
+        ParCarreraMateria[] Lista_nombres = materia_a_borrar.getInfoMateria().getParesCarreraMateria();  //O(1)
+       
+        Lista_enlazada<String> lista = materia_a_borrar.getAlumnos(); //O(1)
+        
+        int cantidad_alumnos_a_desinscribir=lista.longitud(); //O(1)
+        for (int i = 0; i < cantidad_alumnos_a_desinscribir; i++){ //O(Em)
+            
+            String lu = (String)lista.obtenerUltimo(); //O(1)
+            
+            alumnos.obtener(lu).reducirInscripciones(); //O(1)
+            
+            lista.eliminar_ultimo(); //O(1)
         }
-        //O(1)
-        Lista_enlazada<Carrera> carreras_donde_borrar= materia_a_borrar.getCarreras();
-        // Sumartoria para todo n en Nm de |n| + |Nm|
+        
+        Lista_enlazada<Carrera> carreras_donde_borrar= materia_a_borrar.getCarreras(); //O(1)
         int cantidad_de_carreras_a_borrar=carreras_donde_borrar.longitud();
-        for (int i=0; i<cantidad_de_carreras_a_borrar; i++){
-            //O(1)
-            Carrera carrera_actual = carreras_donde_borrar.obtenerUltimo();
-            //O(|Nm|)
-            carrera_actual.getMaterias().borrar(Lista_nombres[i].getNombreMateria());             
-            //O(1)
-            carreras_donde_borrar.eliminar_ultimo();
+        for (int i=0; i<cantidad_de_carreras_a_borrar; i++){ // Sumartoria para todo n en Nm de |n| 
+           
+            Carrera carrera_actual = carreras_donde_borrar.obtenerUltimo(); //O(1)
+            carrera_actual.getMaterias().borrar(Lista_nombres[i].getNombreMateria()); //O(|n|)             
+            carreras_donde_borrar.eliminar_ultimo();  //O(1)
         } 
     }
 
-    //O(|carrera|+|Materia|)
+    //O(|c|+|m|)
     public int inscriptos(String materia, String carrera){
-        //O(|carrera|)
-        Trie <Materia> materias = this.carreras.obtener(carrera).getMaterias();
-        //O(|materia|)
-        int cant_alumnos = materias.obtener(materia).getAlumnos().longitud();
-        //O(1)
+        
+        Trie <Materia> materias = this.carreras.obtener(carrera).getMaterias(); //O(|c|)
+        
+        int cant_alumnos = materias.obtener(materia).getAlumnos().longitud(); //O(|m|)
+        
         return cant_alumnos;
     }
     
-     // O(|carrera|+|materia|)
+     // O(|c|+|m|)
      public boolean excedeCupo(String materia, String carrera){
-        //O(|carrera|+|materia|)
-        int cant_alumnos = inscriptos(materia, carrera);
-        //O(|carrera|+|materia|)
-        int cupo = cupo(materia, carrera);
+        
+        int cant_alumnos = inscriptos(materia, carrera); //O(|c|+|m|)
+        int cupo = cupo(materia, carrera); //O(|c|+|m|)
         return cant_alumnos > cupo;
 
     }
 
     // O(|c|+|m|)
     public int cupo(String materia, String carrera) {
-        // O(|c|+|m|)
-        int[] plantelDocente = plantelDocente(materia, carrera);
-        // O(1)
-        int[] array = new int[4];
-        // O(1)
-        array[0] = plantelDocente[0]*250;
-        // O(1)
-        array[1] = plantelDocente[1]*100;
-        // O(1)
-        array[2] = plantelDocente[2]*20;
-        // O(1)
-        array[3] = 30*plantelDocente[3];
-        // O(1)
+        
+        int[] plantelDocente = plantelDocente(materia, carrera); // O(|c|+|m|)
+        
+        int[] array = new int[4]; // O(1)
+       
+        array[0] = plantelDocente[0]*250; // O(1)
+       
+        array[1] = plantelDocente[1]*100; // O(1)
+        
+        array[2] = plantelDocente[2]*20; // O(1)
+        
+        array[3] = 30*plantelDocente[3]; // O(1)
+       
         int res = array[0];
         // O(1) porque el bucle se repite una cantidad acotada de veces
         for (int i : array) {
-            // O(1)
-            if (i < res) {
-                // O(1)
-                res = i;
+            
+            if (i < res) {  // O(1)
+               
+                res = i;  // O(1)
             }
         }
-        // O(1)
         return res;
     }
     
-    // O(Sumatoria desde 1 a |C| de (|c|)
-    // Con "C" el conjunto de carreras y "c" el nombre (String) de una (cada) carrera.
+    // O(Sumatoria c en |C| de (|c|)
     public String[] carreras(){
-        // O(Sumatoria desde 1 a |C| de (|c|)
+        // O(Sumatoria c a |C| de (|c|)
         // Ver el método ".toStringArray()" de la clase Trie.
         return carreras.toStringArray();
     }
 
-    // O(|c| + Sumatoria desde 1 a |Mc| de (|mc|)
-    // Con "c" el nombre (String) de la carrera pasada por parámetro, "Mc" el conjunto de materias correspondiente a dicha materia, y "mc" el nombre (string) de una (cada) materia.
+    // O(|c| + Sumatoria mc en|Mc| de (|mc|)
     public String[] materias(String carrera){
-        // obtener(c) tiene O(|c|)
-        Carrera objeto_carrera = carreras.obtener(carrera);
-        // O(Sumatoria desde 1 a |Mc| de (|mc|)
+        
+        Carrera objeto_carrera = carreras.obtener(carrera); //O(|c|)
+        // O(Sumatoria mc en Mc de (|mc|)
         // Ver el método ".toStringArray()" de la clase Trie.
         return objeto_carrera.getMaterias().toStringArray();
     }
 
     // O(1)
     public int materiasInscriptas(String estudiante){
-        // O(1) pues la LU de cada estudiante esta acotada
-        Alumno alumno = this.alumnos.obtener(estudiante);
-        // O(1)
-        return alumno.obtenerInscripciones();
+        
+        Alumno alumno = this.alumnos.obtener(estudiante); // O(1) pues la LU de cada estudiante esta acotada
+        return alumno.obtenerInscripciones(); // O(1)
     }
 }
 
