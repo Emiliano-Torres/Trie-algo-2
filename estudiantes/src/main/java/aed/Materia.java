13a0d1b00824e0ded711da4839af1603865a0003
Emiliano-Torres <emilianomtorres1@gmail.com>
2024-06-20 11:27:48 -0300

    archivo materia completo

--- estudiantes/src/main/java/aed/Materia.java
+++ estudiantes/src/main/java/aed/Materia.java
@@ -1,8 +1,8 @@
 package aed;
 /*INV REP : (para todo i entero)(0<=i<=|profesores|->L profesores[i]>=0)
             |infomateria|>0
-            |carreras|>=0
-            |alumnos|>=0
+            |C|>=|carreras|>=0
+            |E|>=|alumnos|>=0
 */
 public class Materia {
     private Lista_enlazada<Carrera> carreras;
@@ -16,23 +16,26 @@ public class Materia {
      * profesores[0]=PROF
      * 
     */
+    //O(1)
     public Materia (InfoMateria info){
         this.info=info;
         carreras= new Lista_enlazada<>();
         alumnos= new Lista_enlazada<>();
         
     }
-    
+    //O(1)
     public Lista_enlazada<String> getAlumnos(){
         return this.alumnos;
     }
+    //O(1)
     public Lista_enlazada<Carrera> getCarreras(){
         return this.carreras;
     }
-
+    //O(1)
     public int[] getProfesores(){
         return this.profesores;        
     }
+    //O(1)
     public InfoMateria getInfoMateria(){
         return this.info;
     }
